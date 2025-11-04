c2de591ebe50d52560a31249a31fbd65337e215b
Moayad Batwa <moayadbatwa@gmail.com>
2025-11-04 03:39:56 +0300

    next1

---
+++ mainImage.py
@@ -0,0 +1,16 @@
+# CPCS331 - Artificial Intelligence
+# Students: Moayad Batwa - Hossam Amasha
+
+from ultralytics import YOLO
+import cv2
+
+# Load a model
+model = YOLO('YOLOv11n.torchscript')  # load a pretrained model
+
+
+# Use the model
+# Upload your image here
+results = model('images/6.jpg', show=True)  # show an image
+
+
+cv2.waitKey(0)
